3b2a0a8d709b63c94bb8358df7ed552bcb371f93
davique0 <davique@gmail.com>
2023-08-08 09:19:26 -0700

    Fix bug ourclasses anchor tag

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -50,7 +50,7 @@ type Props = {
 
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100 py-0">
+    <section id="ourclasses" className="pt-24 w-full bg-primary-100 py-0">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
