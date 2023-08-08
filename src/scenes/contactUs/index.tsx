6c801a42387320e64516681536359bd9dd825b5c
davique0 <davique@gmail.com>
2023-08-08 08:25:19 -0700

    Fix ContacUs bug

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -28,7 +28,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
   }
 
   return (
-    <section id="contacts" className="mx-auto w-5/6 pt-24 pb-32">
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
       >
